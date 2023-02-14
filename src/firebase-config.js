53f23e871427bb1875d49de3f5614c720054fa42
Collection50 <fkaus65@naver.com>
2023-02-14 21:33:29 +0900

    refactor(firebase-config): config 값 오류 해결을 위한 리팩토링

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,10 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from '@firebase/firestore';
-import firebaseConfig from './config/firebase';
+// import firebaseConfig from './config/firebase';
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyALRpkHOUa3PGGtddlO5WzVC959Z-YH9p8',
+  authDomain: 'leets-203bb.firebaseapp.com',
+  projectId: 'leets-203bb',
+  storageBucket: 'leets-203bb.appspot.com',
+  messagingSenderId: '345947995292',
+  appId: '1:345947995292:web:d38d017cf065de92e34159',
+  measurementId: 'G-L2YMPFNE0G',
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
