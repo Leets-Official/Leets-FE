8dd0b4563f041236d6adf86a9642152e2a3855bb
Collection50 <fkaus65@naver.com>
2023-02-18 16:43:45 +0900

    style(viewports): desktop 기준 변경

--- src/utils/viewports.jsx
+++ src/utils/viewports.jsx
@@ -1,4 +1,4 @@
-const breakpoints = [768, 840, 1200];
+const breakpoints = [768, 840, 1180];
 
 const [tablet, middle, desktop] = breakpoints.map(bp => `@media (min-width: ${bp}px)`);
 const mobile = '@media (max-width: 420px)';
