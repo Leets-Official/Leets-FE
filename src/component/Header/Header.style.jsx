a149b94d27728f3479aeb3ee41907c9ded2709c6
Collection50 <fkaus65@naver.com>
2023-02-18 16:15:45 +0900

    refactor: 반응형 css 리팩토링

--- src/component/Header/Header.style.jsx
+++ src/component/Header/Header.style.jsx
@@ -1,10 +1,22 @@
 import { css } from '@emotion/react';
+import { tablet, desktop } from '../../utils/viewports';
 
 export const headContainer = height => css`
   width: 100%;
   height: ${height}px;
 
   line-height: ${height + 200}px;
+
+  ${tablet} {
+    height: ${height + 250}px;
+    line-height: ${height + 450}px;
+  }
+
+  ${desktop} {
+    height: ${height}px;
+    line-height: ${height + 200}px;
+  }
+
   text-align: center;
 `;
 
