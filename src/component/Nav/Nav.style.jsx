9415a1747a602f74e371ca4cc73004cd670bc415
Collection50 <fkaus65@naver.com>
2023-02-20 23:29:13 +0900

    feat(Forms): 지원 페이지 구현

--- src/component/Nav/Nav.style.jsx
+++ src/component/Nav/Nav.style.jsx
@@ -6,17 +6,22 @@ export const navStyle = css`
 
 export const home = css`
   display: flex;
-  justify-content: center;
   align-items: center;
+  background: white;
+  padding-left: 3.81vw;
 
-  width: 145px;
+  width: 100%;
   height: 80px;
 `;
 
 export const linkStyle = css`
+  font-family: 'DM Sans';
+  font-style: normal;
+
   text-decoration: none;
+  background: white;
 
   font-weight: 400px;
   font-size: 26px;
-  color: white;
+  color: black;
 `;
