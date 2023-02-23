a23e1a1b388d38193b5d7106313ab93c0f7fc2ec
Collection50 <fkaus65@naver.com>
2023-02-24 00:42:36 +0900

    refactor(Forms): onChange 핸들러 리팩토링

--- src/component/Forms/Forms.jsx
+++ src/component/Forms/Forms.jsx
@@ -64,7 +64,7 @@ export default function Forms() {
     });
 
     setInfo(initialInfo);
-    alert(`${info.GPA}점 밖에 안 되는 ${info.name}님 if문 3개 제출 완료되었습니다.`);
+    alert(`${info.name}님 제출 완료되었습니다.`);
   };
 
   const clearStorage = () => {
@@ -95,6 +95,12 @@ export default function Forms() {
     localStorage.setItem('tempInfo', JSON.stringify(info));
   };
 
+  const handleOnChange = e => {
+    const { id, value } = e.target;
+    setInfo({ ...info, [id]: value });
+    saveStorage();
+  };
+
   useEffect(() => {
     loadStorage();
   }, []);
@@ -120,10 +126,7 @@ export default function Forms() {
                 id="name"
                 value={info.name}
                 placeholder="이름을 입력하세요."
-                onChange={e => {
-                  setInfo({ ...info, name: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -138,10 +141,7 @@ export default function Forms() {
                 id="SID"
                 value={info.SID}
                 placeholder="학점을 입력하세요."
-                onChange={e => {
-                  setInfo({ ...info, SID: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -157,10 +157,7 @@ export default function Forms() {
                 id="major"
                 value={info.major}
                 placeholder="학과를 입력하세요."
-                onChange={e => {
-                  setInfo({ ...info, major: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -175,10 +172,7 @@ export default function Forms() {
                 id="career"
                 value={info.career}
                 placeholder="희망 직무를 입력하세요. (필수 입력은 아닙니다.)"
-                onChange={e => {
-                  setInfo({ ...info, career: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
               />
             </li>
 
@@ -193,10 +187,7 @@ export default function Forms() {
                 id="GPA"
                 value={info.GPA}
                 placeholder="학점을 입력하세요."
-                onChange={e => {
-                  setInfo({ ...info, GPA: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -211,10 +202,7 @@ export default function Forms() {
                 id="algorithm"
                 value={info.algorithm}
                 placeholder="알고리즘 실력을 입력하세요. ex) 백준/릿코드/프로그래머스"
-                onChange={e => {
-                  setInfo({ ...info, algorithm: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -229,10 +217,7 @@ export default function Forms() {
                 id="project"
                 value={info.project}
                 placeholder="프로젝트 경험을 입력하세요. ex) TODO 프로젝트 배포 1회/기여 내용 등"
-                onChange={e => {
-                  setInfo({ ...info, project: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -248,10 +233,7 @@ export default function Forms() {
                 id="link"
                 value={info.link}
                 placeholder="Github/Figma 주소를 적어주세요."
-                onChange={e => {
-                  setInfo({ ...info, link: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -267,10 +249,7 @@ export default function Forms() {
                 id="phone"
                 value={info.phone}
                 placeholder="전화번호 적어주세요. 개인 정보는 안내 이외에는 사용되지 않습니다."
-                onChange={e => {
-                  setInfo({ ...info, phone: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -283,12 +262,10 @@ export default function Forms() {
               <textarea
                 css={textareaStyle}
                 value={info.goal}
+                id="goal"
                 name="goal"
                 placeholder="내용을 입력해주세요."
-                onChange={e => {
-                  setInfo({ ...info, goal: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -300,12 +277,10 @@ export default function Forms() {
               <textarea
                 css={textareaStyle}
                 value={info.completion}
+                id="completion"
                 name="completion"
                 placeholder="내용을 입력해주세요."
-                onChange={e => {
-                  setInfo({ ...info, completion: e.target.value });
-                  saveStorage();
-                }}
+                onChange={handleOnChange}
                 required
               />
             </li>
@@ -317,12 +292,10 @@ export default function Forms() {
               <textarea
                 css={textareaStyle}
                 value={info.fight}
+                id="fight"
                 name="fight"
                 placeholder="내용을 입력해주세요."
-                onChange={e => {
-                  setInfo({ ...info, fight: e.target.value });
-                  saveStorage();
-                }}
+                onChange={e => handleOnChange(e)}
                 required
               />
             </li>
