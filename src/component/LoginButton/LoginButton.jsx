dfcdb8273ac5a78095ee9cf7e6a7c81e65f38ebb
Collection50 <fkaus65@naver.com>
2023-02-25 22:32:37 +0900

    feat: 구글 로그인 페이지 구현

--- src/component/LoginButton/LoginButton.jsx
+++ src/component/LoginButton/LoginButton.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+/** @jsxImportSource @emotion/react */
+// import { useState } from 'react';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useDispatch } from 'react-redux';
 import { auth } from '../../firebase-config';
 import { userSlice } from '../../features/userSlice';
+import {
+  loginBackground,
+  loginContainer,
+  buttonContainer,
+  imageStyle,
+  textStyle,
+  buttonStyle,
+} from './LoginButton.style';
+import Nav from '../Nav/Nav';
 
 export default function LoginButton() {
-  const [userData, setUserData] = useState(null);
+  // const [userData, setUserData] = useState(null);
   const dispatch = useDispatch();
 
   function handleGoogleLogin() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then(data => {
-        setUserData(data.user);
+        // setUserData(data.user);
         dispatch(
           userSlice.actions.login({
             user: data.user.displayName,
@@ -26,11 +36,20 @@ export default function LoginButton() {
   }
 
   return (
-    <div>
-      <button type="button" onClick={handleGoogleLogin} style={{ background: 'white' }}>
-        구글 로그인
-      </button>
-      <div style={{ color: 'white' }}>{userData ? userData.email : null}</div>
-    </div>
+    <>
+      <Nav />
+
+      <div css={loginBackground}>
+        <div css={loginContainer}>
+          <div css={buttonContainer}>
+            <button type="button" onClick={handleGoogleLogin} css={buttonStyle}>
+              <img src="../assets/image/googleLogo.png" alt="logo" css={imageStyle} />
+              <div css={textStyle}>구글 아이디로 로그인 하기</div>
+            </button>
+          </div>
+          {/* <div style={{ color: 'white' }}>{userData ? userData.email : null}</div> */}
+        </div>
+      </div>
+    </>
   );
 }
---
+++ src/component/LoginButton/LoginButton.style.jsx
@@ -0,0 +1,78 @@
+import { css } from '@emotion/react';
+
+export const loginBackground = css`
+  width: 100%;
+  height: 100vh;
+
+  background: white;
+
+  display: flex;
+  justify-content: center;
+  // border: 1px solid black;
+`;
+
+export const loginContainer = css`
+  width: 45%;
+  height: 100%;
+
+  background: white;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  // border: 1px solid blue;
+`;
+
+export const buttonContainer = css`
+  margin-top: 200px;
+  width: 100%;
+  background: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  // border: 1px solid red;
+`;
+
+export const buttonStyle = css`
+  // margin-top: 200px;
+
+  width: 432px;
+  height: 80px;
+
+  border-radius: 42px;
+
+  background: #e6e6e6;
+
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  // border: 1px solid black;
+  border: none;
+`;
+export const imageStyle = css`
+  width: 50px;
+  height: 50px;
+  background: #e6e6e6;
+
+  // border: 1px solid black;
+`;
+
+export const textStyle = css`
+  background: #e6e6e6;
+
+  margin-left: 40px;
+
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 24px;
+
+  // border: 1px solid blue;
+`;
