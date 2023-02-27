0c77f2717d175cc147d0ff194a0cfa2e97b83bf1
Collection50 <fkaus65@naver.com>
2023-02-27 14:56:46 +0900

    bug: api key 수정

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -11,6 +11,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGEIN_ID,
   appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
