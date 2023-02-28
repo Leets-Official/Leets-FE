70e25db280e33c57f8b48f076b1991bd00f748fb
Collection50 <fkaus65@naver.com>
2023-02-28 17:43:05 +0900

    feat(favicon): favicon 업데이트

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Global } from '@emotion/react';
 import GlobalStyle from './GlobalStyle';
 import Main from './pages/Main';
 import Apply from './pages/Apply';
-import Calculator from './pages/Calculator';
 import Login from './pages/Login';
 
 export default function App() {
@@ -13,7 +12,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Apply" element={<Apply />} />
-        <Route path="/Calculator" element={<Calculator />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
     </>
