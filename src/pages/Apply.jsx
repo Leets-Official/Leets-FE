95f939ed71c1ffd333feccdf11e13482ae2175b8
Collection50 <fkaus65@naver.com>
2023-02-28 17:00:11 +0900

    style: 메인 색상 변경

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -4,7 +4,7 @@ import Nav from '../component/Nav/Nav';
 import Forms from '../component/Forms/Forms';
 import Login from './Login';
 
-const todayColor = 'blue';
+const todayColor = 'green';
 
 export default function Apply() {
   if (!isDesktop()) {
