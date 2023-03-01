3fe9814c28d15524d82d41cfc7d076753dc49483
Collection50 <fkaus65@naver.com>
2023-03-01 21:26:17 +0900

    feat(Footer): 데스크탑의 Footer 구현

---
+++ src/component/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+/** @jsxImportSource @emotion/react */
+
+import { sectionContainer, footerContainer, textStyle, hrStyle } from './Footer.style';
+
+export default function Footer() {
+  return (
+    <section css={sectionContainer}>
+      <div css={footerContainer}>
+        <hr css={hrStyle} />
+        <div css={textStyle}>Copyright 2023. Collection50 all right reseved.</div>
+      </div>
+    </section>
+  );
+}
