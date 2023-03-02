cd0e269b9994cbe04cfcea49fd4c4e4e6096138a
Collection50 <fkaus65@naver.com>
2023-03-02 11:58:27 +0900

    fix: 지원 버튼 활성화

--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -5,13 +5,13 @@ import { isDesktop } from '../../utils/deviceChecker';
 
 export default function Button({ link, title, color }) {
   const handleClick = e => {
-    e.preventDefault();
-    alert('3/2 12시부터 지원 가능합니다.');
+    // e.preventDefault();
+    // alert('3/2 12시부터 지원 가능합니다.');
 
-    // if (!isDesktop()) {
-    //   e.preventDefault();
-    //   alert('지원하기는 PC를 이용해주세요.');
-    // }
+    if (!isDesktop()) {
+      e.preventDefault();
+      alert('지원하기는 PC를 이용해주세요.');
+    }
   };
 
   return (
