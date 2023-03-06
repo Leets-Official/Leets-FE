37a4ec0d0c07da1e5c7c4ccce487e6c0d6d2c376
Collection50 <fkaus65@naver.com>
2023-03-06 23:56:49 +0900

    style: 메인 색상 변경

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,7 @@ import MainContainer from '../component/MainContainer/MainContainer';
 import MobileContainer from '../component/Mobile/MobileContanier/MobileContanier';
 import { isMobile } from '../utils/deviceChecker';
 
-const todayColor = 'blue';
+const todayColor = 'yellow';
 
 export default function Main() {
   // console.log('isMobile(): ', isMobile());
