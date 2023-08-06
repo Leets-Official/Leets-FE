e33577c605e09b875de169cbae155259d1d6596e
Collection50 <fkaus65@naver.com>
2023-08-06 21:53:56 +0900

    refactor: favicon 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,24 @@
 import { ReactNode } from 'react';
+import type { Metadata } from 'next';
 import { RTProvider, StyledProvider } from '@/lib/Provider';
 
-export const metadata = {
-  title: 'Leets',
-  description: 'Leets',
+export const metadata: Metadata = {
+  title: { default: 'Leets', template: '%s | Leets' },
+  description: 'Who Cares?',
+  openGraph: {
+    title: 'Leets',
+    description: 'Who Cares?',
+    images: [
+      {
+        url: '/assets/OG.png',
+      },
+    ],
+    locale: 'ko-KR',
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
