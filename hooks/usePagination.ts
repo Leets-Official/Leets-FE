6fc7154db8d3d953e3f9ab3fa1ec142513bd4543
Collection50 <fkaus65@naver.com>
2023-08-09 23:59:12 +0900

    feat: Pagination을 위한 usePagination hook 구현

---
+++ hooks/usePagination.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+
+type PaginationResult<T> = {
+  currentPage: number;
+  currentItems: T[];
+  handlePageChange: (pageNumber: number) => void;
+};
+
+export function usePagination<T>({ items }: { items: T[] }): PaginationResult<T> {
+  const searchParams = useSearchParams();
+  const pageNumber = searchParams.get('pageNumber');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    setCurrentPage(Number(pageNumber ?? 1));
+  }, [pageNumber]);
+
+  const indexOfLastItem = currentPage * 10;
+  const indexOfFirstItem = indexOfLastItem - 10;
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+  return {
+    currentPage,
+    currentItems,
+    handlePageChange,
+  };
+}
