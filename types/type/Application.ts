5b6f661edb32a40c8a5b37422bda1dc180cff726
Collection50 <fkaus65@naver.com>
2023-08-11 22:00:53 +0900

    feat: 신청서 관련 타입 수정

--- types/type/Application.ts
+++ types/type/Application.ts
@@ -1,22 +1,60 @@
+import { APPLICATION_STATUS_MAP } from '@/constants';
+import { Applicant } from './User';
+
 export type Application = {
   name: string;
-  SID: string;
+  sid: string;
+  gpa: string;
   major: string;
-  career: string;
-  GPA: string;
   algorithm: string;
-  project: string;
+  portfolio: string;
+  position: string;
   link: string;
   phone: string;
   interviewDay: string;
   interviewTime: string;
 
+  enhancement: string;
+  level: string;
+  pros: string;
   goal: string;
   completion: string;
-  fight: string;
 };
 
 export type Input = {
   id: string;
   value: string;
 };
+
+export type IdRequest = { id: string };
+
+export type ApplicationStatus = keyof typeof APPLICATION_STATUS_MAP;
+
+export type GetApplicationResponse = {
+  name: string;
+  gpa: 0;
+  grade: 0;
+  position: string;
+  interview: string;
+  hasInterview: string;
+  applicationStatus: ApplicationStatus;
+};
+
+export type PatchApplication = Application;
+
+export type PostApplication = Application;
+
+export type GetApplicationDetaiResponse = Applicant &
+  Application & {
+    id: number;
+    updatedAt: string;
+    createdAt: string;
+  };
+
+export type PatchApplicationDetailRequest = IdRequest & {
+  applicationStatus: ApplicationStatus;
+};
+
+export type PatchApplicationDetailResponse = GetApplicationDetaiResponse;
+
+export type GetUserApplicationResponse = GetApplicationDetaiResponse;
