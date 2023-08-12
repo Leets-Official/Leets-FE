10577755514d0971879adfbe2bd584616d70a6e1
Collection50 <fkaus65@naver.com>
2023-08-12 19:37:51 +0900

    refactor: 지원서 관련 타입 리팩토링

--- types/type/Application.ts
+++ types/type/Application.ts
@@ -1,25 +1,31 @@
-import { APPLICATION_STATUS_MAP } from '@/constants';
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { POSITION_MAP, APPLICATION_STATUS_MAP, POSITION_ENGLIST_MAP } from '@/constants';
 import { Applicant } from './User';
+import { KeyOf } from '../Helper';
 
-export type Application = {
+export type ApplicationInput = {
   name: string;
   sid: string;
   gpa: string;
   major: string;
   algorithm: string;
+  project: string;
+  career: string;
   portfolio: string;
-  position: string;
-  link: string;
   phone: string;
   interviewDay: string;
   interviewTime: string;
+};
 
+export type ApplicationTextarea = {
   enhancement: string;
   level: string;
   pros: string;
   goal: string;
   completion: string;
 };
+
+export type Application = ApplicationInput & ApplicationTextarea;
 
 export type Input = {
   id: string;
@@ -29,6 +35,10 @@ export type Input = {
 export type IdRequest = { id: string };
 
 export type ApplicationStatus = keyof typeof APPLICATION_STATUS_MAP;
+
+export type GetApplicationRequest = {
+  type: KeyOf<typeof POSITION_MAP>;
+};
 
 export type GetApplicationResponse = {
   name: string;
@@ -40,21 +50,45 @@ export type GetApplicationResponse = {
   applicationStatus: ApplicationStatus;
 };
 
+export type ApplicationListType = GetApplicationResponse;
+
 export type PatchApplication = Application;
 
-export type PostApplication = Application;
+export type PositionType = keyof typeof POSITION_ENGLIST_MAP;
+
+export type PostApplication = Application & {
+  email?: string;
+  position: PositionType;
+};
 
 export type GetApplicationDetaiResponse = Applicant &
   Application & {
     id: number;
     updatedAt: string;
     createdAt: string;
+    interviewDate: string;
+    applicationStatus: ApplicationStatus;
   };
 
-export type PatchApplicationDetailRequest = IdRequest & {
+export type ApplicationDetailType = GetApplicationDetaiResponse;
+
+export type PatchApplicationDetailRequest = {
+  id: number;
   applicationStatus: ApplicationStatus;
+  schedule: string;
 };
 
 export type PatchApplicationDetailResponse = GetApplicationDetaiResponse;
 
 export type GetUserApplicationResponse = GetApplicationDetaiResponse;
+
+export type ApplicationInputProp = {
+  position: PositionType;
+  changeHandler: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, value: any) => void;
+};
+
+export type ApplicationTextareaProp = {
+  position: PositionType;
+  text: ApplicationTextarea;
+  setText: Dispatch<SetStateAction<ApplicationTextarea>>;
+};
