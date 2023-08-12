f6e392c8372ec8137ff5fdc7281bdd73a4bbe9af
Collection50 <fkaus65@naver.com>
2023-08-12 19:35:58 +0900

    refactor: 지원 관련 상수 리팩토링

--- constants/application.ts
+++ constants/application.ts
@@ -1,4 +1,4 @@
-export const APPPLICATION_LIST = {
+export const APPPLICATION_FILTER_LIST = {
   gpa: '학점',
   interviewDate: '면접 일시',
   applicationStatus: '합격 여부',
@@ -9,3 +9,38 @@ export const DETAIL_APPLICATION_DATA = {
   interviewDate: '면접 일시',
   applicationStatus: '합격 여부',
 };
+
+export const APPLICATION_TEXT_DEFAULT = {
+  enhancement: '',
+  level: '',
+  pros: '',
+  goal: '',
+  completion: '',
+};
+
+export const APPLICATION_INPUT_DEFAULT = {
+  sid: '',
+  name: '',
+  email: '',
+  phone: '',
+  gpa: '',
+  major: '',
+  algorithm: '',
+  project: '',
+  career: '',
+  portfolio: '',
+  interviewDay: '',
+  interviewTime: '',
+};
+
+export const APPLICATION_DETAIL_DEFAULT = {
+  uid: '',
+  id: 0,
+  updatedAt: '',
+  createdAt: '',
+  interviewDate: '',
+  applicationStatus: 'PENDING',
+
+  ...APPLICATION_INPUT_DEFAULT,
+  ...APPLICATION_TEXT_DEFAULT,
+} as const;
