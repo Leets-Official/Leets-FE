056d4a263806e6652a025cddce6b94684784f4c2
Collection50 <fkaus65@naver.com>
2023-08-15 15:20:36 +0900

    refactor: useLogin의 로직 구조 변경

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -4,12 +4,12 @@ import * as api from '@/api';
 import { ACCESS_TOKEN, ADMIN, LOGIN } from '@/constants';
 import axios from 'axios';
 import { LocalStorage } from '@/utils';
-import { LoginRequest } from '@/types';
+import { AdminLoginRequest } from '@/types';
 import { useInputRef } from './useInputRef';
 
 const useLogin = () => {
   const router = useRouter();
-  const { inputRef, changeHandler } = useInputRef<LoginRequest>({
+  const { inputRef, changeHandler } = useInputRef<AdminLoginRequest>({
     defaultValues: LOGIN,
   });
 
@@ -17,11 +17,11 @@ const useLogin = () => {
     e.preventDefault();
 
     const { id, password } = inputRef.current;
-    const { result } = await api.postLogin({ id, password });
-    LocalStorage.setItem(ACCESS_TOKEN, result.accessToken);
+    const { result } = await api.postAdminLogin({ id, password });
     if (axios.isAxiosError(result)) {
       return;
     }
+    LocalStorage.setItem(ACCESS_TOKEN, result.accessToken);
     router.replace(ADMIN.HOME);
   };
   return { inputRef, changeHandler, onSubmitHandler };
