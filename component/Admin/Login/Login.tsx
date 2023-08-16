d8701ac1d9d133eb5c5ecca86e0b72b36884763a
Collection50 <fkaus65@naver.com>
2023-08-16 10:51:04 +0900

    feat: 관리자가 로그인 했을 때 로그인 화면에 접속하면 리다이렉트 되도록 변경

--- component/Admin/Login/Login.tsx
+++ component/Admin/Login/Login.tsx
@@ -1,11 +1,18 @@
 import Nav from '@/component/Admin/Nav';
 import { useLogin } from '@/hooks';
-import { LOGIN_LAYOUT } from '@/constants';
+import { ADMIN, LOGIN_LAYOUT } from '@/constants';
+import { useAppSelector } from '@/store';
+import { useRouter } from 'next/navigation';
 import * as S from './Login.styled';
 
 const Login = () => {
   const { changeHandler, onSubmitHandler } = useLogin();
+  const { name } = useAppSelector((state) => state.admin);
+  const router = useRouter();
 
+  if (name) {
+    router.push(ADMIN.HOME);
+  }
   return (
     <S.ApplyListContainer>
       <S.ContentContainer>
