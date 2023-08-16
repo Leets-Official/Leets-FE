cdd8bec51880b452434fcf40390b5d8e31e48730
Collection50 <fkaus65@naver.com>
2023-08-16 10:40:55 +0900

    refactor: 불필요한 api 삭제 및 api 리팩토링

--- api/mail.ts
+++
@@ -1,8 +0,0 @@
-import { http } from '@/api/core';
-import { BaseResponse, IdRequest } from '@/types';
-
-export const postMail = (id: IdRequest): Promise<BaseResponse<boolean>> =>
-  http.post({
-    url: '/application',
-    data: id,
-  });
