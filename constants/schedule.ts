22cc76d93465895fbcd747afca36136e4d7bc0a6
Collection50 <fkaus65@naver.com>
2023-08-18 18:09:27 +0900

    feat: 지원과 관련된 상수 추가

--- constants/schedule.ts
+++ constants/schedule.ts
@@ -2,3 +2,8 @@ export const DEFAULT_TIME = {
   TIME_FORMAT: 'HH:mm',
   FULL_TIME_FORMAT: `YYYY-MM-DD HH:mm`,
 } as const;
+
+export const APPLY_PERIOD = {
+  RECRUIT: 'RECRUIT',
+  CLOSE: 'CLOSE',
+} as const;
