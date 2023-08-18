93f25df8cfe1dee98c6f1e10716bcbd29c9190a7
Collection50 <fkaus65@naver.com>
2023-08-18 18:05:24 +0900

    refactor: ApplyButton 컴포넌트의 라우팅 상수화

--- component/ApplyButton/ApplyButton.tsx
+++ component/ApplyButton/ApplyButton.tsx
@@ -1,9 +1,10 @@
 import { ThemeColor } from '@/types';
+import { USER } from '@/constants';
 import * as S from './ApplyButton.styled';
 
 const ApplyButton = ({ color }: { color: ThemeColor }) => (
   <S.Container>
-    <S.ApplyButton href="/apply" color={color}>
+    <S.ApplyButton href={USER.APPLY} color={color}>
       지원하기
     </S.ApplyButton>
   </S.Container>
