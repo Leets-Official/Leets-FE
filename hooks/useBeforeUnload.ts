90057d3de9439aba87f0cc8982a4db7e2f76e425
Collection50 <fkaus65@naver.com>
2023-08-19 17:15:50 +0900

    feat: 지원서 작성 페이지에서 새로고침, 뒤로가기를 누르는 경우 컨펌을 받는 useBeforeUnload hook 구현

---
+++ hooks/useBeforeUnload.ts
@@ -0,0 +1,25 @@
+import { APPLICATION } from '@/constants';
+import { useState, useEffect } from 'react';
+
+export function useBeforeUnload() {
+  const [submitClicked, setSubmitClicked] = useState<boolean>(false);
+
+  const submitClickHandler = () => {
+    setSubmitClicked(() => true);
+  };
+
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      if (!submitClicked) {
+        e.preventDefault();
+        e.returnValue = APPLICATION.CONFIRM_MOVE;
+      }
+    };
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [submitClicked]);
+
+  return submitClickHandler;
+}
