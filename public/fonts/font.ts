c8324886fc075461cd934c160d4d6cbe53ab0472
Collection50 <fkaus65@naver.com>
2023-08-20 22:11:48 +0900

    style: 로컬 폰트 추가

---
+++ public/fonts/font.ts
@@ -0,0 +1,101 @@
+import localFont from 'next/font/local';
+
+export const NotoSansKR = localFont({
+  src: [
+    {
+      path: './DMSans/DMSans-Thin.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './DMSans/DMSans-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+});
+
+export const Pretendard = localFont({
+  src: [
+    {
+      path: './Pretendard/Pretendard-Thin.woff',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-ExtraLight.woff',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-Light.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-Regular.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-Medium.woff',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-SemiBold.woff',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-ExtraBold.woff',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: './Pretendard/Pretendard-Black.woff',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+});
