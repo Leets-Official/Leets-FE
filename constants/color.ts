8a72bc80a5a65ef9b71e2036e540d9e4eaff3030
Collection50 <fkaus65@naver.com>
2023-09-09 02:17:58 +0900

    refactor: 지원서 상태에 따른 색상 값 추가

--- constants/color.ts
+++ constants/color.ts
@@ -8,7 +8,7 @@ export const BACKGROUND_COLOR = {
   yellow: '#FCB836',
 } as const;
 
-export const BUTTON_COLOR = {
+export const APPLY_BUTTON_COLOR = {
   green: '#10DAB2',
   blue: '#3685FC',
   yellow: '#FCB836',
@@ -18,4 +18,20 @@ export const TEXT_COLOR = {
   green: 'black',
   blue: 'white',
   yellow: 'black',
+} as const;
+
+export const APPLICATION_STATUS_TEXT_COLOR = {
+  PENDING: '#989898',
+  PASS: '#1a9882',
+  PASS_PAPER: '#1a9882',
+  FAIL: '#eb3d4d',
+  FAIL_PAPER: '#eb3d4d',
+} as const;
+
+export const APPLICATION_STATUS_BG_COLOR = {
+  PENDING: '#e9e8e8',
+  PASS: '#e9faf7',
+  PASS_PAPER: '#e9faf7',
+  FAIL: '#feecee',
+  FAIL_PAPER: '#feecee',
 } as const;
