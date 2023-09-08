54c28da25ede60fe45002adb1b890a58116f90e0
Collection50 <fkaus65@naver.com>
2023-09-09 00:01:43 +0900

    refactor: 면접 응시 여부 상태에 PENDING 추가

--- types/type/Application.ts
+++ types/type/Application.ts
@@ -42,7 +42,7 @@ export type GetApplicationRequest = {
   position: KeyOf<typeof POSITION_FILTER_MAP>;
 };
 
-export type InterviewStatusType = 'CHECK' | 'UNCHECK';
+export type InterviewStatusType = 'CHECK' | 'UNCHECK' | 'PENDING';
 
 export type GetApplicationResponse = {
   id: number;
