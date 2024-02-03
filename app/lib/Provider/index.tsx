66ce0e2bd45932f0d76ae47fcfe765c9794233c6
Collection50 <fkaus65@naver.com>
2024-02-03 16:38:12 +0900

    refactor: font에 관련된 폴더를 lib 폴더에서 보관하도록 리팩토링

--- app/lib/Provider/index.tsx
+++ app/lib/Provider/index.tsx
@@ -1,2 +1,3 @@
 export { default as RTProvider } from './RTProvider';
 export { default as StyledProvider } from './StyledProvider';
+export { default as NextAuthProvider } from './SessionProvider';
--- app/lib/index.tsx
+++ app/lib/index.tsx
@@ -1,2 +1,3 @@
 export * from './Provider';
 export * from './authOptions';
+export * from './fonts';
