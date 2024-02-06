4b0ef7cde1958b0421895665b3c2cf2e0958fa61
Collection50 <fkaus65@naver.com>
2024-02-06 21:46:51 +0900

    fix: open graph 이미지 정보 추가

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -5,6 +5,13 @@ import { StyledProvider, DM_SANS } from '@/lib';
 export const metadata: Metadata = {
   title: { default: '관리자', template: '%s · Leets' },
   description: 'Who Cares?',
+  metadataBase: new URL('https://leets.land'),
+  openGraph: {
+    title: 'Leets',
+    description: 'Who Cares?',
+    url: 'https://leets.land',
+    siteName: 'Leets',
+  },
   icons: {
     icon: '/favicon.ico',
   },
