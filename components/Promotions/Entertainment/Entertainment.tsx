34b8fbd37b3b6cd09c4f9e60adb1bc8cc54d630f
Collection50 <fkaus65@naver.com>
2024-02-06 18:16:19 +0900

    refactor: Entertainment  컴포넌를 반응형 컴포넌트로 변경

--- components/Promotions/Entertainment/Entertainment.tsx
+++ components/Promotions/Entertainment/Entertainment.tsx
@@ -1,15 +1,30 @@
+'use client';
+
+import { useViewport } from '@/hooks';
 import * as S from './Entertainment.styled';
 
-const Entertainment = ({ benefits }: { benefits: string[] }) => (
-  <S.ContentContainer>
-    <S.GridContainer>
-      {benefits.map((benefit, index) => (
-        <S.Balloon key={benefit} $index={index}>
-          <S.TextStyle>{benefit}</S.TextStyle>
-        </S.Balloon>
-      ))}
-    </S.GridContainer>
-  </S.ContentContainer>
-);
+const Entertainment = ({ benefits }: { benefits: string[] }) => {
+  const { isDesktop } = useViewport();
+
+  return (
+    <S.ContentContainer>
+      {isDesktop && (
+        <S.GridContainer>
+          {benefits.map((benefit, index) => (
+            <S.Balloon key={benefit} $index={index}>
+              <S.TextStyle>{benefit}</S.TextStyle>
+            </S.Balloon>
+          ))}
+        </S.GridContainer>
+      )}
+      {!isDesktop &&
+        benefits.map((benefit, index) => (
+          <S.BalloonMobile key={benefit} $index={index}>
+            <S.TextStyle>{benefit}</S.TextStyle>
+          </S.BalloonMobile>
+        ))}
+    </S.ContentContainer>
+  );
+};
 
 export default Entertainment;
