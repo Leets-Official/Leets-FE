5c8998eb6ad1408ba0f30d33ea21b6057fcfa54f
Collection50 <fkaus65@naver.com>
2024-02-06 21:18:44 +0900

    style: form 페이지의 주의사항에 애니메이션 추가

--- components/Form/Notice/Notice.tsx
+++ components/Form/Notice/Notice.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { ThemeColor } from '@/types';
 import DropDownArrow from '@/public/assets/image/DropDownArrow.svg';
+import { motion, AnimatePresence } from 'framer-motion';
 import * as S from './Notice.styled';
 
 const Notice = ({ color }: { color: ThemeColor }) => {
@@ -18,18 +19,27 @@ const Notice = ({ color }: { color: ThemeColor }) => {
           <DropDownArrow />
         </S.ImageContainer>
       </S.NoticeTitle>
-      {isToggle && (
-        <S.NoticeTextContainer>
-          <S.Notice>
-            - 제출 시 <S.Highlight>변경하거나 수정</S.Highlight>할 수 없습니다.
-          </S.Notice>
-          <S.Notice>
-            - <S.Highlight>제출하기</S.Highlight>를 클릭해야 최종 지원 처리됩니다.
-          </S.Notice>
-          <S.Notice>- 모든 안내사항은 로그인 시 사용한 메일로 안내됩니다.</S.Notice>
-          <S.Notice>- 선호 일자와 면접 일자는 달라질 수 있습니다.</S.Notice>
-        </S.NoticeTextContainer>
-      )}
+      <AnimatePresence>
+        {isToggle && (
+          <motion.div
+            key="accordionContent"
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3, ease: 'easeInOut' }}>
+            <S.NoticeTextContainer>
+              <S.Notice>
+                - 제출 시 <S.Highlight>변경하거나 수정</S.Highlight>할 수 없습니다.
+              </S.Notice>
+              <S.Notice>
+                - <S.Highlight>제출하기</S.Highlight>를 클릭해야 최종 지원 처리됩니다.
+              </S.Notice>
+              <S.Notice>- 모든 안내사항은 로그인 시 사용한 메일로 안내됩니다.</S.Notice>
+              <S.Notice>- 선호 일자와 면접 일자는 달라질 수 있습니다.</S.Notice>
+            </S.NoticeTextContainer>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </S.NoticeContainer>
   );
 };
