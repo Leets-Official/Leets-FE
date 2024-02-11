242816724e155e7d332944d48e1ad98231e28b94
Collection50 <fkaus65@naver.com>
2024-02-11 16:16:30 +0900

    refactor: layout.tsx를 사용하여 공통 레이아웃을 사용하도록 변경

--- app/(user)/(application)/apply/layout.tsx
+++ app/(user)/(application)/apply/layout.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import * as S from './styled';
 
 export const metadata: Metadata = {
   title: '지원하기',
 };
 
-const ApplyLayout = ({ children }: { children: ReactNode }) => <>{children}</>;
+const ApplyLayout = ({ children }: { children: ReactNode }) => <S.ApplyContainer>{children}</S.ApplyContainer>;
 
 export default ApplyLayout;
