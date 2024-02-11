801504fad12ea8c00b5c5a802d35863fe020deae
Collection50 <fkaus65@naver.com>
2024-02-11 16:19:55 +0900

    refactor: 지원자 상세페이지의 데이터를 SSR로 페치하여 전달하는 방식으로 변경 및 레이아웃을 layout.tsx에서 관리되도록 리팩토링

--- app/(admin)/admin/application/[id]/layout.tsx
+++ app/(admin)/admin/application/[id]/layout.tsx
@@ -1,10 +1,20 @@
 import { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import WithLogout from '@/components/Admin/WithLogout';
+import Nav from '@/components/Admin/Nav';
+import * as S from './styled';
 
 export const metadata: Metadata = {
   title: '지원서 상세',
 };
 
-const ApplicationDetailLayout = ({ children }: { children: ReactNode }) => <>{children}</>;
+const ApplicationDetailLayout = ({ children }: { children: ReactNode }) => (
+  <S.ContentContainer>
+    <WithLogout>
+      <Nav />
+    </WithLogout>
+    {children}
+  </S.ContentContainer>
+);
 
 export default ApplicationDetailLayout;
--- app/(admin)/admin/application/[id]/styled/index.ts
+++ app/(admin)/admin/application/[id]/styled/index.ts
@@ -1,51 +1,10 @@
-import AdminLogo from '@/public/assets/image/Logo/Blue.svg';
+'use client';
+
 import { styled } from 'styled-components';
-
-export const ApplyListContainer = styled.main`
-  width: 100%;
-  height: 100%;
-
-  position: absolute;
-
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  overflow: auto;
-
-  background: #f9f9fc;
-  color: black;
-`;
 
 export const ContentContainer = styled.section`
   width: 95%;
   height: auto;
-`;
-
-export const NavContainer = styled.section`
-  width: 100%;
-
-  display: flex;
-  justify-content: space-between;
-
-  margin-top: 15px;
-`;
-
-export const LogoutContainer = styled.div`
-  width: 60px;
-
-  display: flex;
-  align-items: center;
-`;
-
-export const LogoutButton = styled.button`
-  all: unset;
-
-  color: #4a93ff;
-  cursor: pointer;
-`;
-
-export const AdminLogoContainer = styled(AdminLogo)`
-  margin-right: 10px;
 `;
 
 export const Title = styled.section`
@@ -58,13 +17,6 @@ export const Title = styled.section`
 
   display: flex;
   align-items: center;
-
-  margin-top: 15px;
-`;
-
-export const SearchBar = styled.section`
-  width: 100%;
-  height: 40px;
 
   margin-top: 15px;
 `;
