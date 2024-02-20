cacc31a2ab5eb77edfc0358338b48b3b0de2dfde
Collection50 <fkaus65@naver.com>
2024-02-20 17:22:31 +0900

    feat: ApplicationFilterContext를 반환하는 useApplicationFilterContext 구현

---
+++ hooks/useApplicationFilterContext.ts
@@ -0,0 +1,9 @@
+import { ApplicationFilterContext } from '@/app/lib/Provider/ApplicationFilterProvider';
+import { useContext } from 'react';
+
+const useApplicationFilterContext = () => {
+  const context = useContext(ApplicationFilterContext);
+  return context;
+};
+
+export default useApplicationFilterContext;
