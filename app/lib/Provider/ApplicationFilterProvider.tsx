2345f3c2ca74258a89a4403210e6fb6574d3cfc2
Collection50 <fkaus65@naver.com>
2024-02-21 12:50:55 +0900

    refactor: 신청서 필터링을 사용하기 위한 context에서 쿼리를 관리할 수 있도록 리팩토링

--- app/lib/Provider/ApplicationFilterProvider.tsx
+++ app/lib/Provider/ApplicationFilterProvider.tsx
@@ -1,18 +1,20 @@
 'use client';
 
 import { APPLICATION_DEFAULT_FILTER_CONDITION } from '@/constants';
-import { ReactNode, createContext, useState, Dispatch, SetStateAction, useMemo } from 'react';
+import { ReactNode, createContext, useMemo } from 'react';
+import useQuery from '../../../hooks/useQuery';
 
 interface ApplicationFilter {
-  filterCondition: typeof APPLICATION_DEFAULT_FILTER_CONDITION;
-  setFilterCondition: Dispatch<SetStateAction<typeof APPLICATION_DEFAULT_FILTER_CONDITION>>;
+  query: typeof APPLICATION_DEFAULT_FILTER_CONDITION;
+  setQuery: <V extends object>(object: V, predicate?: boolean) => void;
+  initQuery: () => void;
 }
 
 export const ApplicationFilterContext = createContext<ApplicationFilter | null>(null);
 
 const ApplicationFilterProvider = ({ children }: { children: ReactNode }) => {
-  const [filterCondition, setFilterCondition] = useState(APPLICATION_DEFAULT_FILTER_CONDITION);
-  const contextValue = useMemo(() => ({ filterCondition, setFilterCondition }), [filterCondition, setFilterCondition]);
+  const { query, setQuery, initQuery } = useQuery(APPLICATION_DEFAULT_FILTER_CONDITION);
+  const contextValue = useMemo(() => ({ query, setQuery, initQuery }), [query, setQuery, initQuery]);
 
   return <ApplicationFilterContext.Provider value={contextValue}>{children}</ApplicationFilterContext.Provider>;
 };
