61ece60a676d1ff60ae03c2e19ada573e44d84db
One-HyeWon <5876675@naver.com>
2025-08-21 22:04:29 +0900

    style: 스크롤바 스타일 없애기

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -7,9 +7,29 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html, body, #__next {
+  html {
     height: 100%;
     background: black;
     font-size: 62.5%;
+    overflow: -moz-scrollbars-none; /* Firefox */
+    -ms-overflow-style: none; /* IE 10+ */
+    scrollbar-width: none; /* Firefox */
+    
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  body {
+    height: 100%;
+    background: black;
+    
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+
+  #__next {
+    height: 100%;
   }
 `;
